9fd12706319745c5938800fcbf4825ece851e109
Adavize Hassan <adavize302@gmail.com>
2025-08-15 13:45:54 +0100

    feat: 🎸 add session composable

--- frontend/composables/auth/use-login.ts
+++ frontend/composables/auth/use-login.ts
@@ -2,8 +2,11 @@ import { useMutation } from "@tanstack/vue-query";
 import { authControllerLogin } from "~/client/sdk.gen";
 import type { LoginRequestDto } from "~/client/types.gen";
 import { useApiClient } from "~/composables/api/use-api-client";
+import useSession from "~/composables/session/use-session";
 
 export const useLogin = () => {
+  const { setSessionStorage } = useSession();
+
   const mutation = useMutation({
     mutationKey: ["loginUser"],
     mutationFn: async (payload: LoginRequestDto) =>
@@ -13,6 +16,7 @@ export const useLogin = () => {
         body: payload,
       }),
     onSuccess: (response) => {
+      setSessionStorage("tkn", response.data?.access_token as string);
       return response;
     },
     onError: (error) => {
